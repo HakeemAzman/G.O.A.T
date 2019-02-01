6c14d1fb1291d983f83e6674b23a39bc7670c287
Jesper <leonrulez1@gmail.com>
2019-02-01 10:24:44 +0800

    Encyclopedia + Main Menu

---
+++ G.O.A.T/Assets/Encyclopedia.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Encyclopedia : MonoBehaviour {
+
+    public GameObject blinkingText;
+
+	// Use this for initialization
+	void Start () {
+        //StartCoroutine("StopBlinking");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void BackToGame()
+    {
+        SceneManager.LoadScene("MainLevel");
+    }
+
+    /*
+    IEnumerator StopBlinking()
+    {
+        blinkingText.SetActive(true);
+
+        yield return new WaitForSeconds(5f);
+
+        blinkingText.SetActive(false);
+        Debug.Log("Blinking Text off");
+    }
+    */
+}
--- G.O.A.T/Assets/G.O.A.T/MainMenu V2 Script/SettingsMenu.cs
+++ G.O.A.T/Assets/G.O.A.T/MainMenu V2 Script/SettingsMenu.cs
@@ -7,13 +7,57 @@ public class SettingsMenu : MonoBehaviour {
 
     public AudioMixer audioMixer;
 
+    public GameObject muteAudioButton;
+    public GameObject unmuteAudioButton;
+    bool audioMute = false;
+
+    AudioSource aS;
+
+    void Start()
+    {
+        aS = GetComponent<AudioSource>();
+        audioMute = false;
+    }
+
+    void Update()
+    {
+        if (audioMute == false)
+        {
+            muteAudioButton.SetActive(true);
+            unmuteAudioButton.SetActive(false);
+        }
+        else
+        {
+            unmuteAudioButton.SetActive(true);
+            muteAudioButton.SetActive(false);
+        }
+    }
+
+    public void MuteAudio()
+    {
+        audioMute = true;
+        unmuteAudioButton.SetActive(true);
+        muteAudioButton.SetActive(false);
+        Debug.Log("Muted");
+    }
+
+    public void UnmuteAudio()
+    {
+        audioMute = false;
+        unmuteAudioButton.SetActive(false);
+        muteAudioButton.SetActive(true);
+        Debug.Log("Unmuted");
+    }
+
+
+
+
     public void SetVolume (float volume)
     {
         audioMixer.SetFloat("volume", volume);
         Debug.Log(volume);
     }
-
-
+    
     public void SetFullScreen (bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
