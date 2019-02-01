f8112ba1a3da0c6e2560d248c76ed88fef2c6bc2
HakeemAzman <nurhakeemazman@gmail.com>
2019-02-01 19:01:19 +0800

    Water Reflection

---
+++ G.O.A.T/Assets/G.O.A.T/Script/Defences/Fish Dispenser/BadFish.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadFish : MonoBehaviour
+{
+    public float destroyTimer;
+    public Poison poiScript;
+
+    public bool hasTouched = false;
+
+    private void Start()
+    {
+        destroyTimer = poiScript.poisonTimer;
+    }
+
+    private void OnTriggerEnter(Collider enter)
+    {
+        if(enter.gameObject.tag == "Enemies")
+        {
+            hasTouched = true;
+            Destroy(this.gameObject, destroyTimer);
+        }
+    }
+}
---
+++ G.O.A.T/Assets/G.O.A.T/Script/Defences/Fish Dispenser/FishDispenser.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishDispenser : MonoBehaviour
+{
+    #region Feesh
+    [Header ("Fish Dispenser Setup")]
+    public Transform spawnHere;
+    public GameObject prefabBadFish;
+    private GameObject badFish;
+
+    [Header ("Fish Dispenser Settings")]
+    public float rateOfSpawn;
+
+    private float spawnTimer;
+
+    private void Start()
+    {
+        spawnTimer = rateOfSpawn;
+    }
+
+    private void Update()
+    {
+        if (badFish == null)
+        {
+            spawnTimer -= Time.deltaTime;
+
+            if (spawnTimer <= 0)
+            {
+                spawnTimer = rateOfSpawn;
+                badFish = Instantiate(prefabBadFish, spawnHere.position, spawnHere.rotation);
+            }
+        }
+    }
+    #endregion
+}
