b3e2733931e3684c69cfd76b01d73ec71d10f0cc
Nick Lee <44768335+SmallRunt@users.noreply.github.com>
2019-02-08 03:52:29 +0800

    Tutorial Level Done
    It should encompass all the required knowledge for the game.

--- G.O.A.T/Assets/TutorialManager.cs
+++ G.O.A.T/Assets/TutorialManager.cs
@@ -9,12 +9,18 @@ public class TutorialManager : MonoBehaviour {
     bool tutorial1 = true;
     bool tutorial2 = true;
     bool tutorial3 = true;
+    bool tutorial4 = true;
+    bool upgradebool = true;
     public GameObject tutorialBox;
     public Text tutorialText;
     public Drawer drawRibbon;
     public Selecting Sscript;
     public Deployment dpScript;
     public BtnManager btnScript;
+    public TutorialUpgrade tscript;
+    public Animator blinkingDrawer;
+    public Animator blinkingDeploy;
+    public Animator blinkingReady;
     // Use this for initialization
     void Start () {
 		
@@ -40,18 +46,22 @@ public class TutorialManager : MonoBehaviour {
 
         if (!tutorial1)
         {
-            tutorialText.text = ("Click on the ribbon to see your currency and life.");
+            blinkingDrawer.SetBool("tutorialOn", true);
+            tutorialText.text = ("Click on the ribbon to see your currency and life. You can also click on it again to close it");
             tutorialBox.SetActive(true);
+            
         }
 
         if(drawRibbon.canOpen == true)
         {
+            blinkingDrawer.SetBool("tutorialOn", false);
             tutorialText.text = ("The bubbles are your currency for building defenses and the fishes are your lives.");
             tutorial2 = false;
         }
 
         if(!tutorial2)
         {
+            blinkingDeploy.SetBool("isDeploy", true);
             tutorialText.text = ("Click on the icebergs to deploy a turret! They will cost you some bubbles!");
             if(Sscript.hasHit)
             {
@@ -61,6 +71,7 @@ public class TutorialManager : MonoBehaviour {
 
         if (dpScript.hasDeploy == true)
         {
+            blinkingDeploy.SetBool("isDeploy", false);
             tutorialText.text = ("You have now deployed a snowball turret! Now you can fend off those pesky penguins from your fishes!");
             tutorialBox.SetActive(true);
             StartCoroutine(timeDelay());
@@ -74,19 +85,46 @@ public class TutorialManager : MonoBehaviour {
 
         if(startButton)
         {
-            tutorialText.text = ("Click on the Penguin button on the top left to begin the wave!");
-            tutorialBox.SetActive(true);  
+            blinkingReady.SetBool("isReady", true);
+            tutorialText.text = ("Click on the Penguin button on the top left to begin the wave whenever you're done with deploying your defenses!");
+            tutorialBox.SetActive(true);
+            
+        }
+        if(btnScript.isReady == true)
+        {
+            tutorial4 = false;
+            
         }
 
-        if(btnScript.isReady)
+        if(!tutorial4)
+        {
+            tutorialBox.SetActive(false);
+            StartCoroutine(timeToWait());
+        }
+        if(!upgradebool)
+        {
+            tutorialText.text = ("Click on Upgrade in the scroll to raise the tower! Once you rise up high enough, a Boss will spawn and once you kill it, you win!");
+            tutorialBox.SetActive(true);
+           
+        }
+        
+        if(tscript.isUpgraded == true)
         {
             tutorialBox.SetActive(false);
         }
+        
     }
 
     IEnumerator timeDelay()
     {
         yield return new WaitForSeconds(5f);
         tutorial3 = false;
+    }
+
+    IEnumerator timeToWait()
+    {
+        yield return new WaitForSeconds(30f);
+        upgradebool = false;
+
     }
 }
